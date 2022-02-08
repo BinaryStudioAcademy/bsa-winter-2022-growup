8fd4bd39b598b28d188c6c39bb5a548bf5fcce5c
Karinkaa <kharinakaryna@gmail.com>
2022-02-08 17:35:25 +0200

    fix: updated app component

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -1,20 +1,48 @@
 import { AppRoute } from 'common/enums/enums';
-import { Route, Routes } from 'components/common/common';
-import Counter from 'components/counter/counter';
+import { Link, Route, Routes } from 'components/common/common';
+import HomePage from 'components/home-page/home-page';
 import Login from 'components/login/login';
 import Ork from 'components/okrs/okrs';
 import Profile from 'components/profile/profile';
 import SignUp from 'components/sign-up/sign-up';
+import { useLocation } from 'hooks/hooks';
 
 const App: React.FC = () => {
+  const { pathname } = useLocation();
+
   return (
-    <Routes>
-      <Route path={AppRoute.ROOT} element={<Counter />} />
-      <Route path={AppRoute.LOGIN} element={<Login />} />
-      <Route path={AppRoute.SIGN_UP} element={<SignUp />} />
-      <Route path={AppRoute.ORKS} element={<Ork />} />
-      <Route path={AppRoute.SETTINGS_PROFILE} element={<Profile />} />
-    </Routes>
+    <div className="App">
+      <div>
+        <ul className="App-navigation-list">
+          <li>
+            <Link to={AppRoute.ROOT}>Root</Link>
+          </li>
+          <li>
+            <Link to={AppRoute.LOGIN}>Login</Link>
+          </li>
+          <li>
+            <Link to={AppRoute.SIGN_UP}>Sign up</Link>
+          </li>
+          <li>
+            <Link to={AppRoute.ORKS}>Ork</Link>
+          </li>
+          <li>
+            <Link to={AppRoute.PAGES}>Pages</Link>
+          </li>
+          <li>
+            <Link to={AppRoute.SETTINGS_PROFILE}>Profile Settings</Link>
+          </li>
+        </ul>
+        <p>Current path: {pathname}</p>
+      </div>
+      <Routes>
+        <Route path={AppRoute.ROOT} element={<HomePage />} />
+        <Route path={AppRoute.LOGIN} element={<Login />} />
+        <Route path={AppRoute.SIGN_UP} element={<SignUp />} />
+        <Route path={AppRoute.ORKS} element={<Ork />} />
+        <Route path={AppRoute.SETTINGS_PROFILE} element={<Profile />} />
+      </Routes>
+    </div>
   );
 };
 
