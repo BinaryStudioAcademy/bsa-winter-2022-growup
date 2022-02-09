c00437b0b17ce43998f12acbc7a17dda88c684dd
twistedomelette <54509271+twistedomelette@users.noreply.github.com>
2022-02-09 15:09:25 +0200

    fix: changed dimension

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -90,6 +90,8 @@ const App: React.FC = () => {
         </header>
         <input type="button" value="Click" className="btn-gu-purple"/>
         <div className="bg-gu-black text-gu-white fs-2">LAlmost before we knew it, we had left the ground.</div>
+        <div className="bg-gu-black text-gu-white fs-3">LAlmost before we knew it, we had left the ground.</div>
+        <div className="bg-gu-black text-gu-white fs-4">LAlmost before we knew it, we had left the ground.</div>
       </div>
     </>
   );
