ddc529636bb3f9a2c9f83f52a2f9b49b0493ab0b
Aleksandr Kukharenko <oleksandr.kukharenko@binary-studio.com>
2022-02-10 23:39:44 +0200

    fix: path to public

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -16,7 +16,7 @@ const { port } = env.app;
 const app: Express = express();
 
 app.use(cors());
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, '../public')));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
