ee1f349e0b22207889f33e5ae56ba48195a81d92
Alexandr Smirnov <https://github.com/Alexandr21Smirnov/my-resume.git>
2022-02-10 21:49:35 +0200

    fix: immplement new funcionality

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -7,37 +7,48 @@ import Profile from 'components/profile/profile';
 import Admin from 'components/admin/admin';
 import Header from 'components/header/header';
 import Sidebar from 'components/sidebar/sidebar';
+import Main_Page from 'components/main-page/main-page';
+import './app.scss';
 
 const App: React.FC = () => {
   const isAdmin = false;
+
   return (
-      <>
-        <Sidebar />
-        <Header />
-            {isAdmin? (
-              <Routes>
-                <Route path={AppRoute.ADMIN} element={<Admin variant="company" />} />
-                <Route path={AppRoute.ADMIN_USERS} element={<Admin variant="users" />} />
-                <Route
-                  path={AppRoute.ADMIN_CAREER_PATH}
-                  element={<Admin variant="career" />}
-                />
-              </Routes>
-            ):(
-              <Routes>
-                <Route path={AppRoute.LOGIN} element={Login} />
-                <Route path={AppRoute.SIGN_UP} element={SignUp} />
-                <Route path={AppRoute.ORKS} element={Ork} />
-                <Route path={AppRoute.SETTINGS_PROFILE} element={Profile} />
-                <Route path={AppRoute.HOME} element={<div>Home</div>} />
-                <Route path={AppRoute.PROFILE} element={<div>Profile</div>} />
-                <Route path={AppRoute.CAREER_PATH} element={<div>Career Path</div>} />
-                <Route path={AppRoute.OKR} element={<div>OKR</div>} />
-                <Route path={AppRoute.OPPORTUNITIES} element={<div>Opportunities</div>} />
-                <Route path={AppRoute.MENTEE_PROFILES} element={<div>Mentee Profiles</div>} />
-              </Routes>
-            )}
-      </>
+      <div className="">
+        {isAdmin ? null:
+        <>
+          <Sidebar />
+          <Header />
+        </>
+        }
+        <div className="d-flex">
+              {isAdmin ? (
+                <Routes>
+                  <Route path={AppRoute.ADMIN} element={<Admin variant="company" />} />
+                  <Route path={AppRoute.ADMIN_USERS} element={<Admin variant="users" />} />
+                  <Route
+                    path={AppRoute.ADMIN_CAREER_PATH}
+                    element={<Admin variant="career" />}
+                  />
+                </Routes>
+              ):(
+              <main className="main-container w-100 px-5 pt-3 pb-5">
+                  <Routes>
+                    <Route path={AppRoute.LOGIN} element={Login} />
+                    <Route path={AppRoute.SIGN_UP} element={SignUp} />
+                    <Route path={AppRoute.ORKS} element={Ork} />
+                    <Route path={AppRoute.SETTINGS_PROFILE} element={Profile} />
+                    <Route path={AppRoute.HOME} element={<Main_Page />} />
+                    <Route path={AppRoute.PROFILE} element={<div>Profile</div>} />
+                    <Route path={AppRoute.CAREER_PATH} element={<div>Career Path</div>} />
+                    <Route path={AppRoute.OKR} element={<div>OKR</div>} />
+                    <Route path={AppRoute.OPPORTUNITIES} element={<div>Opportunities</div>} />
+                    <Route path={AppRoute.MENTEE_PROFILES} element={<div>Mentee Profiles</div>} />
+                  </Routes>
+              </main>
+              )}
+          </div>
+      </div>
   );
 };
 
