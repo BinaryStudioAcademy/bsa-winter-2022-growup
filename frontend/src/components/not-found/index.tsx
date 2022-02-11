614770c4a26202162c49a53aea5f6006b8cdae6f
Pavel Kramarenko <pavelk@sweetsoft.com.ua>
2022-02-11 12:51:45 +0200

    fix according comments

--- frontend/src/components/not-found/index.tsx
+++ frontend/src/components/not-found/index.tsx
@@ -13,10 +13,10 @@ const NotFound: FC = () => {
     <Card className="w-100 d-flex align-items-center ">
       <Card.Body className="d-flex flex-column align-items-center justify-content-center">
         <Card.Title className="text-center gu-black">404</Card.Title>
-        <Card.Text className="mb-2 text-center mt-0 text1">
+        <Card.Text className="mb-2 text-center mt-0 fs-1">
           Page not found.
         </Card.Text>
-        <Card.Text className="mb-2 mt-1 text-center text2">
+        <Card.Text className="mb-2 mt-1 text-center fs-3">
           The page you are looking for might have been removed.
         </Card.Text>
         <Button className="mt-3" variant="primary" onClick={onClick}>
