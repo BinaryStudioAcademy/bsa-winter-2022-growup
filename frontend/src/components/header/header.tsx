ff976d7933a57ef4c09c6276e7d190ab42a7b6f4
Alexandr Smirnov <https://github.com/Alexandr21Smirnov/my-resume.git>
2022-02-11 13:31:00 +0200

    fix: add bootstrap styles for header section

--- frontend/src/components/header/header.tsx
+++ frontend/src/components/header/header.tsx
@@ -8,13 +8,13 @@ const Header: React.FC = () => {
 
       return (
           <header className="header-section w-100 position-fixed">
-              <div className="header-container d-flex align-items-center justify-content-end">
+              <div className="header-container d-flex align-items-center justify-content-end pe-4 py-2 pe-md-5 py-md-3"  >
                 <form className="form-section" method="get" action="#">
-                    <img className="form-section__search-icon search-icon position-absolute" src={searchIcon} alt="search icon" />
-                    <input className="form-section__search-input search-input fs-7 border-0 gu-black font-weight-normal" placeholder="Search..." type="text" name="search" />
+                    <img className="form-section__search-icon search-icon ms-3 ms-md-4 position-absolute" src={searchIcon} alt="search icon" />
+                    <input className="form-section__search-input search-input me-4 ps-5 py-4 fs-7 border-0 gu-black font-weight-normal" placeholder="Search..." type="text" name="search" />
                 </form>
                 <div className="notification-section position-relative">
-                    <img className="notification-section__icon notification-icon" src={notificationIcon} alt="notification icon" />
+                    <img className="notification-section__icon notification-icon me-4" src={notificationIcon} alt="notification icon" />
                     <img className="notification-section__notification-pointer notification-pointer position-absolute" src={notificationPointer} alt="notification pointer" />
                 </div>
                 <img className="avatar-icon" src={avatarIcon} alt="avatar icon" />
