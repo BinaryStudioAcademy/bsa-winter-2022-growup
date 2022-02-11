5c9eef7c1c3cae5a3b02e924a920e6d50b734bae
Pavel Kramarenko <pavelk@sweetsoft.com.ua>
2022-02-11 10:30:06 +0200

    update api frontend & add funtionals to AddCompany component

--- frontend/src/components/admin/components/companies/addCompany/index.tsx
+++ frontend/src/components/admin/components/companies/addCompany/index.tsx
@@ -1,5 +1,7 @@
-import React, { FC } from 'react';
+import React, { ChangeEvent, FC, useState } from 'react';
 import { CloseButton, Form, Button, Modal } from 'react-bootstrap';
+import { ICompany } from 'common/interfaces/company/company';
+import { companyApi } from 'services/company-api';
 
 import './styles.scss';
 
@@ -9,6 +11,26 @@ interface IAddCompany {
 }
 
 const AddCompany: FC<IAddCompany> = ({ show, handleClose }) => {
+  const [company, setCompany] = useState<ICompany | unknown>({});
+
+  const onChange = (e: ChangeEvent<HTMLInputElement>): void => {
+    const { value, name } = e.target;
+    const newCompany = Object.assign(company, { [name]: value });
+    setCompany(newCompany);
+  };
+
+  const send = (): void => {
+    const arr = Object.entries(company as ICompany);
+    const index = arr.findIndex((val: [string, string]) => val[1] === '');
+
+    if (index === -1) {
+      companyApi.addCompany(company as ICompany);
+      return;
+    }
+
+    alert('Field ' + arr[index] + ' is empty!!!');
+  };
+
   return (
     <Modal show={show} onHide={handleClose} centered={true}>
       <Modal.Header>
@@ -16,18 +38,28 @@ const AddCompany: FC<IAddCompany> = ({ show, handleClose }) => {
         <CloseButton onClick={handleClose} />
       </Modal.Header>
       <Modal.Body>
-        <Form.Group className="mb-3" controlId="formBasicEmail">
+        <Form.Group className="mb-3" controlId="CompanyName">
           <Form.Label>Company name</Form.Label>
-          <Form.Control type="text" placeholder="Company name" />
+          <Form.Control
+            type="text"
+            placeholder="Company name"
+            onChange={onChange}
+            name="name"
+          />
         </Form.Group>
 
-        <Form.Group className="mb-3" controlId="formBasicPassword">
+        <Form.Group className="mb-3" controlId="comapnyDescription">
           <Form.Label>Description</Form.Label>
-          <Form.Control type="text" placeholder="Description" />
+          <Form.Control
+            type="text"
+            placeholder="Description"
+            onChange={onChange}
+            name="description"
+          />
         </Form.Group>
       </Modal.Body>
       <Modal.Footer>
-        <Button variant="gu-pink" type="submit">
+        <Button variant="gu-pink" type="submit" onClick={send}>
           Save
         </Button>
       </Modal.Footer>
--- frontend/src/services/company-api/index.ts
+++ frontend/src/services/company-api/index.ts
@@ -1,43 +1,55 @@
 import { ICompany } from 'common/interfaces/company/company';
 import { Http } from '../http/http.service';
 import { HttpMethod } from 'common/enums/http/http';
+import { ContentType } from 'common/enums/file/file';
+
 import baseUrl from 'common/baseUrl';
 
 class CompanyApi {
   // eslint-disable-next-line
   private http = new Http();
 
-  public async addCompany(company: ICompany): Promise<ICompany> {
+  public async addCompany(company: ICompany): Promise<ICompany | null> {
     const options = {
       method: HttpMethod.POST,
-      ContentType: 'application/json',
+      contentType: ContentType.JSON,
       payload: JSON.stringify(company),
     };
 
-    const result = await this.http.load<ICompany>(
-      baseUrl + '/company',
-      options,
-    );
-    console.warn(result);
-    return new Promise((resolve) => setTimeout(() => resolve(result), 1000));
+    try {
+      const result = await this.http.load<ICompany>(
+        baseUrl + '/company',
+        options,
+      );
+      return result;
+    } catch (e) {
+      //passing an error to the handler
+      console.warn(e);
+      return null;
+    }
   }
 
-  public async editCompany(company: ICompany): Promise<ICompany> {
+  public async editCompany(company: ICompany): Promise<ICompany | null> {
     const { id } = company;
     delete company.id;
 
     const options = {
-      method: HttpMethod.POST,
-      ContentType: 'application/json',
+      method: HttpMethod.PATCH,
+      contentType: ContentType.JSON,
       payload: JSON.stringify(company),
     };
 
-    const result = await this.http.load<ICompany>(
-      baseUrl + '/company/' + id,
-      options,
-    );
-    console.warn(result);
-    return new Promise((resolve) => setTimeout(() => resolve(result), 1000));
+    try {
+      const result = await this.http.load<ICompany>(
+        baseUrl + '/company/' + id,
+        options,
+      );
+      return result;
+    } catch (e) {
+      //passing an error to the handler
+      console.warn(e);
+      return null;
+    }
   }
 }
 
