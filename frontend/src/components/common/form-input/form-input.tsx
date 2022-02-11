2e0f3f2e4af4c3c2a98585dcec17013d53d8c5ae
Karinkaa <kharinakaryna@gmail.com>
2022-02-11 16:38:56 +0200

    fix: updated types

--- frontend/src/components/common/form-input/form-input.tsx
+++ frontend/src/components/common/form-input/form-input.tsx
@@ -1,26 +1,33 @@
 import { ErrorMessage } from '@hookform/error-message';
 import { Form } from 'react-bootstrap';
-import { Control, useController } from 'react-hook-form';
+import { Control, useController, UseControllerProps } from 'react-hook-form';
 import './styles.scss';
 
 interface Props {
-  name: string,
-  control: Control<object, object>,
-  errors: object,
-  type: string,
-  placeholder: string
+  name: string;
+  control: Control<object, object>;
+  errors: object;
+  type: string;
+  placeholder: string;
 }
 
-const FormInput = ({ name, control, errors, type, placeholder }: Props): JSX.Element => {
-  const { field } = useController<any>({ name, control });
+type FieldType = Pick<Props, 'name' | 'control'>;
+
+const FormInput = ({
+  name,
+  control,
+  errors,
+  type,
+  placeholder,
+}: Props): JSX.Element => {
+  const { field } = useController<UseControllerProps<FieldType>>({
+    name: name,
+    control,
+  });
 
   return (
     <>
-      <Form.Control
-        {...field}
-        type={type}
-        placeholder={placeholder}
-      />
+      <Form.Control {...field} type={type} placeholder={placeholder} />
       <span className="fs-6 text-gu-pink error">
         <ErrorMessage errors={errors} name={name} />
       </span>
