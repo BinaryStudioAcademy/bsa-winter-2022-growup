e1d365227c3a09806b0e13ad23420e854947e177
twistedomelette <54509271+twistedomelette@users.noreply.github.com>
2022-02-13 23:42:55 +0200

    feat: styles for blocks

--- frontend/src/components/okrs/components/okr-item/workspace-item.tsx
+++ frontend/src/components/okrs/components/okr-item/workspace-item.tsx
@@ -1,4 +1,6 @@
 import '../../styles.scss';
+import calendar from 'assets/img/okr-items/calendar-orange.png';
+import pencil from 'assets/img/okr-items/pencil.png';
 
 interface PropTypes {
     name: string
@@ -10,20 +12,26 @@ interface PropTypes {
 
 const OrkItem: React.FC<PropTypes> = (props) => (
 <div className="okr-container bg-white">
-    <div className="OKR-name fs-2">
+    <div className="OKR-name fs-2 mt-2 fw-bold">
       {props.name}
     </div>
-    <div className="reached fs-5">
+    <div className="reached fs-5 fw-bold">
       0.85
     </div>
-    <div className="objectives fs-6">
+    <div className="objectives fs-6 mb-4 fw-bold">
       Objectives: {props.NumOfObjectives},
     </div>
-    <div className="key-result fs-6">
+    <div className="key-result fs-6 fw-bold">
       KeyResults: {props.NumOfKeyResults}
     </div>
-    <div className="timestamp fs-6">
-        {props.startDate} - {props.endDate}
+    <div className="timestamp fs-6 mt-3 d-flex justify-content-evenly">
+      <div>
+        <img alt="calendar" src={calendar} width="12" className="mb-1"/>
+      </div>
+      <div>{props.startDate} - {props.endDate}</div>
+      <div>
+        <img alt="pencil" src={pencil} width="12" className="mb-1"/>
+      </div>
     </div>
 </div>
 );
