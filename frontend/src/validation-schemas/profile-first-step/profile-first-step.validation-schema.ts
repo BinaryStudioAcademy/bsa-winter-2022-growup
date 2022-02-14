778a07db627e88219581d8fcc7d960e6eabe50f8
karyna <kharinakaryna@gmail.com>
2022-02-15 00:06:05 +0200

    feat: added validation schema for the first step of profile settings

---
+++ frontend/src/validation-schemas/profile-first-step/profile-first-step.validation-schema.ts
@@ -0,0 +1,69 @@
+import { FirstStepPayloadKey, UserPayloadKey, UserValidationMessage, UserValidationRule } from 'common/enums/enums';
+import { StepsValidationRule } from '../../common/enums/validation/steps-validation-rule.enum';
+import { StepsValidationMessage } from '../../common/enums/validation/steps-validation.enum';
+import * as Joi from 'joi';
+
+const profileFirstStep = Joi.object({
+  [UserPayloadKey.FIRST_NAME]: Joi.string()
+    .trim()
+    .min(UserValidationRule.FIRST_NAME_MIN_LENGTH)
+    .max(UserValidationRule.FIRST_NAME_MAX_LENGTH)
+    .required()
+    .messages({
+      'string.empty': UserValidationMessage.FIRST_NAME_REQUIRE,
+      'string.min': UserValidationMessage.FIRST_NAME_MIN_LENGTH,
+      'string.max': UserValidationMessage.FIRST_NAME_MAX_LENGTH,
+    }),
+  [UserPayloadKey.LAST_NAME]: Joi.string()
+    .trim()
+    .min(UserValidationRule.LAST_NAME_MIN_LENGTH)
+    .max(UserValidationRule.LAST_NAME_MAX_LENGTH)
+    .required()
+    .messages({
+      'string.empty': UserValidationMessage.LAST_NAME_REQUIRE,
+      'string.min': UserValidationMessage.LAST_NAME_MIN_LENGTH,
+      'string.max': UserValidationMessage.LAST_NAME_MAX_LENGTH,
+    }),
+  [FirstStepPayloadKey.POSITION]: Joi.string()
+    .trim()
+    .min(StepsValidationRule.POSITION_MIN_LENGTH)
+    .max(StepsValidationRule.POSITION_MAX_LENGTH)
+    .required()
+    .messages({
+      'string.empty': StepsValidationMessage.POSITION_REQUIRE,
+      'string.min': StepsValidationMessage.POSITION_MIN_LENGTH,
+      'string.max': StepsValidationMessage.POSITION_MAX_LENGTH,
+    }),
+  [FirstStepPayloadKey.EXPERIENCE]: Joi.string()
+    .trim()
+    .min(StepsValidationRule.EXPERIENCE_MIN_LENGTH)
+    .max(StepsValidationRule.EXPERIENCE_MAX_LENGTH)
+    .required()
+    .messages({
+      'string.empty': StepsValidationMessage.EXPERIENCE_REQUIRE,
+      'string.min': StepsValidationMessage.EXPERIENCE_MIN_LENGTH,
+      'string.max': StepsValidationMessage.EXPERIENCE_MAX_LENGTH,
+    }),
+  [FirstStepPayloadKey.EDUCATION]: Joi.string()
+    .trim()
+    .min(StepsValidationRule.EDUCATION_MIN_LENGTH)
+    .max(StepsValidationRule.EDUCATION_MAX_LENGTH)
+    .required()
+    .messages({
+      'string.empty': StepsValidationMessage.EDUCATION_REQUIRE,
+      'string.min': StepsValidationMessage.EDUCATION_MIN_LENGTH,
+      'string.max': StepsValidationMessage.EDUCATION_MAX_LENGTH,
+    }),
+  [FirstStepPayloadKey.INTERESTING_TAGS]: Joi.string()
+    .trim()
+    .min(StepsValidationRule.INTERESTING_TAGS_MIN_LENGTH)
+    .max(StepsValidationRule.INTERESTING_TAGS_MAX_LENGTH)
+    .required()
+    .messages({
+      'string.empty': StepsValidationMessage.INTERESTING_TAGS_REQUIRE,
+      'string.min': StepsValidationMessage.INTERESTING_TAGS_MIN_LENGTH,
+      'string.max': StepsValidationMessage.INTERESTING_TAGS_MAX_LENGTH,
+    }),
+});
+
+export { profileFirstStep };
