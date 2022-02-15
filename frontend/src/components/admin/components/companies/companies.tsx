6abdf0f1aa7780cd3ca7dfd525a94e229c11af0c
Pavel Kramarenko <pavelk@sweetsoft.com.ua>
2022-02-15 10:33:10 +0200

    revert

--- frontend/src/components/admin/components/companies/companies.tsx
+++ frontend/src/components/admin/components/companies/companies.tsx
@@ -1,19 +1,13 @@
 import { memo } from 'react';
 import { useAppSelector, useEffect, useAppDispatch } from 'hooks/hooks';
 import { adminActions } from 'store/actions';
-import { useSelector } from 'hooks/hooks';
-import { RootState } from 'common/types/types';
 
-import CompanyList from './company-list';
+import Company from './company-item';
 import Tags from './tags/tags';
 
 const Companies: React.FC = () => {
   const { tags } = useAppSelector((state) => state.admin);
   const dispatch = useAppDispatch();
-
-  const { companies } = useSelector(({ companies }: RootState) => ({
-    companies: companies.companies,
-  }));
 
   useEffect(() => {
     dispatch(adminActions.fetchTags());
@@ -22,7 +16,7 @@ const Companies: React.FC = () => {
   return (
     <>
       <div className="col col-sm-8 col-md-8 col-lg-8">
-        <CompanyList companyList={companies} />
+        <Company companyList={[]} />
       </div>
       <div className="col col-sm-4 col-md-4 col-lg-4">
         <Tags tagList={tags} />
