72e1d2a9e543bca5b80cf7bb42fb101b739ccf91
twistedomelette <54509271+twistedomelette@users.noreply.github.com>
2022-02-15 12:14:28 +0200

    improvement

--- frontend/src/components/okr/tabs/two-tabs.tsx
+++ frontend/src/components/okr/tabs/two-tabs.tsx
@@ -1,32 +1,15 @@
 import React, { useState } from 'react';
 import Tabs from 'react-bootstrap/Tabs';
 import Tab from 'react-bootstrap/Tab';
-import OrkItem from './workspace-item';
 import { useSelector } from 'react-redux';
 import { RootState } from 'common/types/types';
-import { IOkr } from './common/interfaces';
-
-interface PropTypes {
-  collection: IOkr[]
-}
+import OkrList from './okr-list';
 
 function ControlledTabs():React.ReactElement {
     const [key, setKey] = useState('my-OKR');
     const user = useSelector((state: RootState) => state.okr.user);
     const okr = useSelector((state: RootState) => state.okr.okr);
     const myOkr = useSelector((state: RootState) => state.okr.okr.filter(okr => okr.userId === user.id));
-
-    const OkrList: React.FC<PropTypes> = (props) => (
-      <div className="OKR-page d-flex flex-row flex-wrap">
-        {props.collection.map((okr: IOkr) => {
-            const collection = okr.objective;
-            const objectivesCounter = collection.length;
-            const resultsCounter = okr.keyResult.length;
-            return <OrkItem key={okr.id} name={okr.name} endDate={okr.endDate}
-              startDate={okr.startDate} objectivesCounter={objectivesCounter} resultsCounter={resultsCounter}/>;
-        })}
-      </div>
-    );
 
     return (
       <Tabs
