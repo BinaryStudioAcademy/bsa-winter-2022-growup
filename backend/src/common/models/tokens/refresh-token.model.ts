41fac6aef0f4cad9b91ec7fe77ba188f3f42addf
Alexandr Smirnov <https://github.com/Alexandr21Smirnov/my-resume.git>
2022-02-16 14:43:57 +0200

    fix: change refresh token schema

--- backend/src/common/models/tokens/refresh-token.model.ts
+++ backend/src/common/models/tokens/refresh-token.model.ts
@@ -1,7 +1,3 @@
-import { RefreshToken } from '~/data/entities/refresh-token';
-import { User } from '~/data/entities/user';
-
 export type refreshTokenSchema = {
-    user: User,
-    token: RefreshToken,
+    refreshToken: string,
 };
