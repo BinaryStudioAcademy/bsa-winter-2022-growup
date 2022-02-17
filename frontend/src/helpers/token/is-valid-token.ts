921c81488844ffea97397d91266d6106e77ec055
karyna <kharinakaryna@gmail.com>
2022-02-17 22:37:54 +0200

    fix: removed unused variable

--- frontend/src/helpers/token/is-valid-token.ts
+++ frontend/src/helpers/token/is-valid-token.ts
@@ -4,8 +4,7 @@ const isValidToken = (token: string): boolean => {
   try {
     parseJwt(token);
     return true;
-    // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  } catch (err) {
+  } catch {
     return false;
   }
 };
