2a1fbb313a77faf1bff7ee246819e2e52f03f559
twistedomelette <54509271+twistedomelette@users.noreply.github.com>
2022-02-17 16:35:08 +0200

    fix: skill route

--- frontend/src/components/profile/components/profile-info/add-section/add-section.tsx
+++ frontend/src/components/profile/components/profile-info/add-section/add-section.tsx
@@ -1,6 +1,5 @@
 import './add-section.scss';
 import { Plus } from 'react-bootstrap-icons';
-import { Link } from 'react-router-dom';
 type Props = {
   title: string;
 };
@@ -9,14 +8,13 @@ const AddSection: React.FC<Props> = ({ children, title }) => (
   <div className="add-section bg-white">
     <div className="add-section-header text-white d-flex justify-content-between align-items-center bg-gu-blue">
       <p className="add-section-header__title m-0 fw-bold fs-4">{title}</p>
-      <Link
-        to="/skill"
+      <button
         type="button"
         className="add-section-header__add fw-bold bg-transparent d-flex align-items-center fs-5 text-gu-white"
       >
         <Plus className="add-section-header__add-icon" />
         <span>Add experience</span>
-      </Link>
+      </button>
     </div>
     <div className="add-section-content position-relative">{children}</div>
   </div>
--- frontend/src/components/profile/components/profile-info/edit-section/edit-section.tsx
+++ frontend/src/components/profile/components/profile-info/edit-section/edit-section.tsx
@@ -1,5 +1,6 @@
 import './edit-section.scss';
 import { PencilFill } from 'react-bootstrap-icons';
+import { Link } from 'react-router-dom';
 
 type Props = {
   title: string;
@@ -9,10 +10,13 @@ const EditSection: React.FC<Props> = ({ children, title }) => (
   <div className="edit-section bg-white">
     <div className="edit-section-header d-flex justify-content-between align-items-center">
       <p className="edit-section-header__title m-0 fw-bold fs-4">{title}</p>
-      <button className="edit-section-header__edit fw-bold bg-transparent d-flex align-items-center fs-5">
+      <Link
+        to="/skill"
+        className="edit-section-header__edit fw-bold bg-transparent d-flex align-items-center fs-5"
+      >
         <PencilFill className="edit-section-header__edit-icon" />
         <span>Edit</span>
-      </button>
+      </Link>
     </div>
     <div className="edit-section-content d-flex flex-wrap align-items-start">
       {children}
