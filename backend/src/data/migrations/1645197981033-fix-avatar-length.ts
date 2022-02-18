f82ba211ed90f7d79e98e9a8101c88cee80e3a26
Iskenderov Viacheslav <s25112003@gmail.com>
2022-02-18 18:37:03 +0200

    fix: migration fixed

--- backend/src/data/migrations/1645197981033-fix-avatar-length.ts
+++ backend/src/data/migrations/1645197981033-fix-avatar-length.ts
@@ -4,12 +4,10 @@ export class fixAvatarLength1645197981033 implements MigrationInterface {
     name = 'fixAvatarLength1645197981033';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query('ALTER TABLE "user" DROP COLUMN "avatar"');
-        await queryRunner.query('ALTER TABLE "user" ADD "avatar" character varying(150)');
+        await queryRunner.query('ALTER TABLE "user" ALTER COLUMN "avatar" TYPE varchar(150)');
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query('ALTER TABLE "user" DROP COLUMN "avatar"');
-        await queryRunner.query('ALTER TABLE "user" ADD "avatar" character varying(100)');
+        await queryRunner.query('ALTER TABLE "user" ALTER COLUMN "avatar" TYPE varchar(100)');
     }
 }
