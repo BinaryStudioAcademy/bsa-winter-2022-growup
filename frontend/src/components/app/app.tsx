01e1b62fa1082da16b1fec05af8b791c827cbd99
Aleksandr Kukharenko <oleksandr.kukharenko@binary-studio.com>
2022-02-18 14:08:39 +0200

    fix: remove unused

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -1,16 +1,4 @@
 import { useAppSelector } from 'hooks/hooks';
-import { AppRoute, ProfileSettingsRoute, RoleType } from 'common/enums/enums';
-import Login from 'components/login/login';
-import SignUp from 'components/sign-up/sign-up';
-import { Navigate, Route, Routes } from 'components/common/common';
-import Ork from 'components/okr/okr';
-import Profile from 'components/profile/profile';
-import { NotFound } from 'components/not-found';
-import Admin from 'components/admin/admin';
-import Header from 'components/header/header';
-import Sidebar from 'components/sidebar/sidebar';
-import Main_Page from 'components/main-page/main-page';
-import ProfileSettings from '../profile-settings/profile-settings';
 import AdminRouting from './admin-routing';
 import UserRouting from './user-routing';
 import './app.scss';
