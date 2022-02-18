4cf5e93aee3a9a7e230c192339248351ec033831
Aleksandr Kukharenko <oleksandr.kukharenko@binary-studio.com>
2022-02-18 14:20:11 +0200

    fix: add missing import

--- frontend/src/components/app/app.tsx
+++ frontend/src/components/app/app.tsx
@@ -1,6 +1,7 @@
 import { useAppSelector } from 'hooks/hooks';
 import AdminRouting from './admin-routing';
 import UserRouting from './user-routing';
+import { RoleType } from 'common/enums/enums';
 import './app.scss';
 
 const App: React.FC = () => {
