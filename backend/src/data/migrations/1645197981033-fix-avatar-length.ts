5c37f71991d4941bf46a841c071fc01d6a6140b1
Iskenderov Viacheslav <s25112003@gmail.com>
2022-02-18 17:35:38 +0200

    fix: linter errors fixed

--- backend/src/data/migrations/1645197981033-fix-avatar-length.ts
+++ backend/src/data/migrations/1645197981033-fix-avatar-length.ts
@@ -1,15 +1,15 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class fixAvatarLength1645197981033 implements MigrationInterface {
-    name = 'fixAvatarLength1645197981033'
+    name = 'fixAvatarLength1645197981033';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "avatar"`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "avatar" character varying(150)`);
+        await queryRunner.query('ALTER TABLE "user" DROP COLUMN "avatar"');
+        await queryRunner.query('ALTER TABLE "user" ADD "avatar" character varying(150)');
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "user" DROP COLUMN "avatar"`);
-        await queryRunner.query(`ALTER TABLE "user" ADD "avatar" character varying(100)`);  
+        await queryRunner.query('ALTER TABLE "user" DROP COLUMN "avatar"');
+        await queryRunner.query('ALTER TABLE "user" ADD "avatar" character varying(100)');
     }
 }
