fe8f8b11c6731ab3677d11a5fe4dad9e4326004b
Alexandr Smirnov <https://github.com/Alexandr21Smirnov/my-resume.git>
2022-02-22 01:56:42 +0200

    fix: add interests section

--- frontend/src/components/profile/components/profile-info/profile-main/profile-main.tsx
+++ frontend/src/components/profile/components/profile-info/profile-main/profile-main.tsx
@@ -1,15 +1,13 @@
-import { CareerJourney, Education, Interests, Skill } from '../interfaces';
+import { CareerJourney, Education, Skill } from '../interfaces';
 import AddSection from '../add-section/add-section';
 import CareerCard from '../career-card/career-card';
 import EditSection from '../edit-section/edit-section';
 import EducationCard from '../education-card/education-card';
 import Tag from '../tag/tag';
 import './profile-main.scss';
-import { useState, useCallback, useEffect } from 'hooks/hooks';
-import TagModal from './tags/modal/tag-modal';
-import { PencilFill } from 'react-bootstrap-icons';
-import { adminActions } from 'store/actions';
-import { useAppDispatch } from 'hooks/store/store.hooks';
+import Interests from './interests/interests-section';
+import { tagsActions } from 'store/actions';
+import { useAppSelector, useEffect, useAppDispatch } from 'hooks/hooks';
 
 // FROM DB
 const careerJourneyData: CareerJourney[] = [
@@ -55,32 +53,12 @@ const skillData: Skill[] = [
   },
 ];
 
-// FROM DB
-const interestsData: Interests[] = [
-  {
-    id: '1',
-    name: 'Lviv',
-  },
-  {
-    id: '2',
-    name: 'Remote',
-  },
-  {
-    id: '3',
-    name: 'JS Developer',
-  },
-];
-
 const ProfileMain: React.FC = () => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  const { tags } = useAppSelector((state) => state.tags);
   const dispatch = useAppDispatch();
 
-  const hideModal = useCallback(() => {
-    setIsModalVisible(false);
-  }, []);
-  const showModal = (): void => setIsModalVisible(true);
   useEffect(() => {
-    dispatch(adminActions.fetchTags());
+    dispatch(tagsActions.fetchTags());
   }, [dispatch]);
 
   return (
@@ -125,29 +103,7 @@ const ProfileMain: React.FC = () => {
             <Tag>French</Tag>
           </div>
         </EditSection>
-        <TagModal show={isModalVisible} onClose={hideModal} />
-        <div className="edit-section bg-white">
-          <div className="edit-section-header d-flex justify-content-between align-items-center">
-            <h3 className="edit-section-header__title m-0 fw-bold fs-4">
-              Interests
-            </h3>
-            <button
-              className="edit-section-header__edit bg-transparent fw-bold d-flex align-items-center fs-5"
-              onClick={showModal}
-            >
-              <PencilFill className="edit-section-header__edit-icon me-2" />
-              <span>Edit</span>
-            </button>
-          </div>
-          <div className="edit-section-content d-flex flex-wrap align-items-start">
-            <div className="group fw-bold fs-7">
-              {interestsData.map((item, i) => (
-                <Tag key={i}>{item.name}</Tag>
-              ))}
-              <div className="d-flex flex-wrap gap-2"></div>
-            </div>
-          </div>
-        </div>
+        <Interests tagList={tags} />
       </div>
     </main>
   );
