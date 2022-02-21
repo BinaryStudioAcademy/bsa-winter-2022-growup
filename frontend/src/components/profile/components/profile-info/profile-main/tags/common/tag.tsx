a36114b994d73c742a3d90782649cd1aeda3c966
Alexandr Smirnov <https://github.com/Alexandr21Smirnov/my-resume.git>
2022-02-22 01:58:10 +0200

    fix: add tag item

--- frontend/src/components/profile/components/profile-info/profile-main/tags/common/tag.tsx
+++ frontend/src/components/profile/components/profile-info/profile-main/tags/common/tag.tsx
@@ -10,7 +10,7 @@ type PropTypes = {
 
 const Tag: React.FC<PropTypes> = ({ tag, onDelete }) => {
   return (
-    <div className="row tag g-0">
+    <div className="row tag bg-gu-white text-gu-blue fs-6 g-0">
       <div className="col">
         {tag.name}{' '}
         <Icon.XLg
