30d751a5e3d86607367342b9ecdd923a3482fbdb
Iskenderov Viacheslav <s25112003@gmail.com>
2022-02-21 19:56:54 +0200

    feat: .vscode added to gitignore

--- backend/src/data/migrations/1645452346700-objective-unique-constraint.ts
+++ backend/src/data/migrations/1645452346700-objective-unique-constraint.ts
@@ -1,14 +1,14 @@
-import {MigrationInterface, QueryRunner} from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class objectiveUniqueConstraint1645452346700 implements MigrationInterface {
-    name = 'objectiveUniqueConstraint1645452346700'
+    name = 'objectiveUniqueConstraint1645452346700';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "skill_objective" ADD CONSTRAINT "skill_unique_objective" UNIQUE ("name", "categoryId")`);
+        await queryRunner.query('ALTER TABLE "skill_objective" ADD CONSTRAINT "skill_unique_objective" UNIQUE ("name", "categoryId")');
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`ALTER TABLE "skill_objective" DROP CONSTRAINT "skill_unique_objective"`);
+        await queryRunner.query('ALTER TABLE "skill_objective" DROP CONSTRAINT "skill_unique_objective"');
     }
 
 }
