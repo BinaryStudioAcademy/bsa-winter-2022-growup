3bc5e9fb94919d92e8512b7963cade57e820644e
karyna <kharinakaryna@gmail.com>
2022-02-22 19:35:03 +0200

    feat: created career journey validation enums

---
+++ frontend/src/common/enums/validation/career-journey-validation-rule.enum.ts
@@ -0,0 +1,10 @@
+const CareerJourneyValidationRuleEnum = {
+  POSITION_MIN_LENGTH: 2,
+  POSITION_MAX_LENGTH: 30,
+  COMPANY_MIN_LENGTH: 2,
+  COMPANY_MAX_LENGTH: 30,
+  START_DATE: '01-01-2000',
+  END_DATE: '22-02-2022',
+};
+
+export { CareerJourneyValidationRuleEnum };
