5208e967a5562289e42c8ed6dfd098115fca28fa
karyna <kharinakaryna@gmail.com>
2022-02-22 19:55:38 +0200

    fix: updated career journey validation enums

--- frontend/src/common/enums/validation/career-journey-validation-rule.enum.ts
+++ frontend/src/common/enums/validation/career-journey-validation-rule.enum.ts
@@ -1,10 +1,8 @@
-const CareerJourneyValidationRuleEnum = {
+const CareerJourneyValidationRule = {
   POSITION_MIN_LENGTH: 2,
   POSITION_MAX_LENGTH: 30,
   COMPANY_MIN_LENGTH: 2,
   COMPANY_MAX_LENGTH: 30,
-  START_DATE: '01-01-2000',
-  END_DATE: '22-02-2022',
 };
 
-export { CareerJourneyValidationRuleEnum };
+export { CareerJourneyValidationRule };
--- frontend/src/common/enums/validation/career-journey-validation.enum.ts
+++ frontend/src/common/enums/validation/career-journey-validation.enum.ts
@@ -1,16 +1,14 @@
-import { CareerJourneyValidationRuleEnum } from './career-journey-validation-rule.enum';
+import { CareerJourneyValidationRule } from './career-journey-validation-rule.enum';
 
-const CareerJourneyValidationEnum = {
+const CareerJourneyValidationMessage = {
   POSITION_REQUIRE: 'Position is required',
-  POSITION_MIN_LENGTH: `Position must be at least ${CareerJourneyValidationRuleEnum.POSITION_MIN_LENGTH} characters long`,
-  POSITION_MAX_LENGTH: `Position must be at most ${CareerJourneyValidationRuleEnum.POSITION_MAX_LENGTH} characters long`,
+  POSITION_MIN_LENGTH: `Position must be at least ${CareerJourneyValidationRule.POSITION_MIN_LENGTH} characters long`,
+  POSITION_MAX_LENGTH: `Position must be at most ${CareerJourneyValidationRule.POSITION_MAX_LENGTH} characters long`,
   COMPANY_REQUIRE: 'Company is required',
-  COMPANY_MIN_LENGTH: `Company must be at least ${CareerJourneyValidationRuleEnum.COMPANY_MIN_LENGTH} characters long`,
-  COMPANY_MAX_LENGTH: `Company must be at most ${CareerJourneyValidationRuleEnum.COMPANY_MAX_LENGTH} characters long`,
+  COMPANY_MIN_LENGTH: `Company must be at least ${CareerJourneyValidationRule.COMPANY_MIN_LENGTH} characters long`,
+  COMPANY_MAX_LENGTH: `Company must be at most ${CareerJourneyValidationRule.COMPANY_MAX_LENGTH} characters long`,
   START_DATE_REQUIRE: 'Start date is required',
-  START_DATE: `Start date must be at least ${CareerJourneyValidationRuleEnum.START_DATE}`,
   END_DATE_REQUIRE: 'End date is required',
-  END_DATE: `End date must be at most ${CareerJourneyValidationRuleEnum.END_DATE}`,
 };
 
-export { CareerJourneyValidationEnum };
+export { CareerJourneyValidationMessage };
