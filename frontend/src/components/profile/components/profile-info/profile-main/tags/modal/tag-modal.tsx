95dde3df821bd28cdc80d63806dc03e73ec98b15
Alexandr Smirnov <https://github.com/Alexandr21Smirnov/my-resume.git>
2022-02-23 16:12:09 +0200

    fix: change tag modal file

--- frontend/src/components/profile/components/profile-info/profile-main/tags/modal/tag-modal.tsx
+++ frontend/src/components/profile/components/profile-info/profile-main/tags/modal/tag-modal.tsx
@@ -1,9 +1,11 @@
-import { FormEvent } from 'react';
-import { useAppDispatch, useTagList } from 'hooks/hooks';
+import { FormEvent, useCallback } from 'react';
+import { useAppDispatch, useAppSelector, useTagList } from 'hooks/hooks';
 import { tagsActions } from 'store/actions';
 import { Modal } from 'components/common/common';
 import TagForm from './form';
 import TagList from './tag-list';
+import InterestsTagList from './interests-tag-list';
+import { ITag } from 'common/interfaces/tag/tag';
 
 type PropTypes = {
   show: boolean;
@@ -11,6 +13,7 @@ type PropTypes = {
 };
 
 const TagModal: React.FC<PropTypes> = ({ show, onClose }) => {
+  const { tags } = useAppSelector((state) => state.tags);
   const { list: tagList, addItem, deleteItem, clearItems } = useTagList();
   const dispatch = useAppDispatch();
   const clickHandler = (e: FormEvent): void => {
@@ -19,6 +22,10 @@ const TagModal: React.FC<PropTypes> = ({ show, onClose }) => {
     clearItems();
     dispatch(tagsActions.createTags(tagList));
   };
+
+  const deleteTag = useCallback((id: ITag['id']) => {
+    dispatch(tagsActions.deleteTag(id));
+  }, []);
 
   return (
     <Modal
@@ -29,6 +36,7 @@ const TagModal: React.FC<PropTypes> = ({ show, onClose }) => {
     >
       <TagForm onSubmit={addItem} />
       <TagList tagList={tagList} onDelete={deleteItem} />
+      <InterestsTagList tagList={tags} onDelete={deleteTag} />
       <button
         className="btn btn-outline-gu-purple btn-hover-gu-white fw-bold fs-5 border-2"
         onClick={clickHandler}
