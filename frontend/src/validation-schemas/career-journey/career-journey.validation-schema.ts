2eb6ad7a892cb28c39c3812abe84f4f8c8a36ea2
karyna <kharinakaryna@gmail.com>
2022-03-09 20:16:22 +0200

    fix: updated career journey validation schema

--- frontend/src/validation-schemas/career-journey/career-journey.validation-schema.ts
+++ frontend/src/validation-schemas/career-journey/career-journey.validation-schema.ts
@@ -1,9 +1,13 @@
 import * as Joi from 'joi';
-import { CareerJourneyPayloadKey } from '../../common/enums/user/career-journey-payload-key.enum';
+import { CareerJourneyPayloadKey } from 'common/enums/user/career-journey-payload-key.enum';
+import {
+  MAX_DATE,
+  MIN_DATE,
+} from 'components/profile/components/profile-edit/common/constants';
 import {
   CareerJourneyValidationMessage,
   CareerJourneyValidationRule,
-} from '../../common/enums/validation/validation';
+} from 'common/enums/validation/validation';
 
 const careerJourney = Joi.object({
   [CareerJourneyPayloadKey.POSITION]: Joi.string()
@@ -25,6 +29,24 @@ const careerJourney = Joi.object({
       'string.empty': CareerJourneyValidationMessage.COMPANY_REQUIRE,
       'string.min': CareerJourneyValidationMessage.COMPANY_MIN_LENGTH,
       'string.max': CareerJourneyValidationMessage.COMPANY_MAX_LENGTH,
+    }),
+  [CareerJourneyPayloadKey.START_DATE]: Joi.date()
+    .required()
+    .min(MIN_DATE)
+    .max(MAX_DATE)
+    .messages({
+      'date.base': CareerJourneyValidationMessage.START_DATE_REQUIRE,
+      'date.min': CareerJourneyValidationMessage.MIN_START_DATE,
+      'date.max': CareerJourneyValidationMessage.MAX_START_DATE,
+    }),
+  [CareerJourneyPayloadKey.END_DATE]: Joi.date()
+    .required()
+    .min(Joi.ref(CareerJourneyPayloadKey.START_DATE))
+    .max(MAX_DATE)
+    .messages({
+      'date.base': CareerJourneyValidationMessage.END_DATE_REQUIRE,
+      'date.min': CareerJourneyValidationMessage.MIN_END_DATE,
+      'date.max': CareerJourneyValidationMessage.MAX_END_DATE,
     }),
 });
 
