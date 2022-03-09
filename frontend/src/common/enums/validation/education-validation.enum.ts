80c87189c52108411b9ed0eaa6ad0bf390e576a4
karyna <kharinakaryna@gmail.com>
2022-03-09 20:15:25 +0200

    fix: updated education validation messages

--- frontend/src/common/enums/validation/education-validation.enum.ts
+++ frontend/src/common/enums/validation/education-validation.enum.ts
@@ -1,4 +1,9 @@
+import { formatDate } from 'helpers/format-date';
 import { EducationValidationRule } from './education-validation-rule.enum';
+import {
+  MAX_DATE,
+  MIN_DATE,
+} from 'components/profile/components/profile-edit/common/constants';
 
 const EducationValidationMessage = {
   SPECIALIZATION_REQUIRE: 'Specialization is required',
@@ -11,7 +16,11 @@ const EducationValidationMessage = {
   DEGREE_MIN_LENGTH: `Degree must be at least ${EducationValidationRule.DEGREE_MIN_LENGTH} characters long`,
   DEGREE_MAX_LENGTH: `Degree must be at most ${EducationValidationRule.DEGREE_MAX_LENGTH} characters long`,
   START_DATE_REQUIRE: 'Start date is required',
+  MIN_START_DATE: `Start date must be at least ${formatDate(MIN_DATE)}`,
+  MAX_START_DATE: 'Start date must be less than or equal to end date',
   END_DATE_REQUIRE: 'End date is required',
+  MIN_END_DATE: 'End date must be greater than or equal to start date',
+  MAX_END_DATE: `End date must be at most ${formatDate(MAX_DATE)}`,
 };
 
 export { EducationValidationMessage };
