60e8c1dc7bc0e819e93be001b733604a6ec261e6
karyna <kharinakaryna@gmail.com>
2022-03-09 20:18:48 +0200

    feat: created form input date component

--- frontend/src/components/common/common.ts
+++ frontend/src/components/common/common.ts
@@ -4,6 +4,6 @@ export { default as Modal } from './modal/modal';
 export { default as UserAvatar } from './avatar/avatar';
 export { default as TextField } from './text-field/text-field';
 export { default as FormInput } from './form-input/form-input';
+export { default as FormInputDate } from './form-input-date/form-input-date';
 export { default as Button } from './button/button';
 export { default as AutoComplete } from './auto-complete/auto-complete';
-export { default as DatePicker } from './date-picker/date-picker';
