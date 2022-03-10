f9dcf2af5422a31eabf2cc8f55eef606678044ad
karyna <kharinakaryna@gmail.com>
2022-03-10 22:43:40 +0200

    fix: updated methods and got data from the state

--- frontend/src/components/profile/components/profile-info/profile-main/career-journey-section.tsx
+++ frontend/src/components/profile/components/profile-info/profile-main/career-journey-section.tsx
@@ -1,49 +1,85 @@
-import { useCallback, useState } from 'hooks/hooks';
+import { NotificationManager } from 'react-notifications';
+import {
+  useAppDispatch,
+  useAppSelector,
+  useCallback,
+  useEffect,
+  useNavigate,
+  useState,
+} from 'hooks/hooks';
 import AddSection from '../add-section/add-section';
 import CareerCard from '../career-card/career-card';
 import { CareerJourneyForm } from '../../profile-edit/profile-edit';
+import {
+  createCareerJourney,
+  getAllCareerJourneys,
+  updateCareerJourney,
+} from 'store/career-journey/actions';
 import { CareerJourney } from '../interfaces';
-
-const careerJourneyData: CareerJourney[] = [
-  {
-    id: '1',
-    position: 'Fullstack JS Developer',
-    company: 'Binary Studio',
-    startDate: new Date('2020-01-30T03:24:00'),
-    endDate: new Date(),
-  },
-  {
-    id: '2',
-    position: 'Fullstack JS Developer',
-    company: 'Binary Studio',
-    startDate: new Date('2021-12-17T03:24:00'),
-    endDate: new Date(),
-  },
-];
+import { MentorMenteeRoute } from 'common/enums/mentor-mentee-route/mentor-mentee-route.enum';
 
 const CareerJourneySection: React.FC = () => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const careerJourneyData = useAppSelector(
+    (state) => state.careerJourney.careerJourneys,
+  );
 
-  const closeModal = useCallback(() => setIsModalVisible(false), []);
-  const showModal = useCallback(() => setIsModalVisible(true), []);
+  useEffect(() => {
+    dispatch(getAllCareerJourneys());
+  }, []);
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [careerJourney, setCareerJourney] = useState<CareerJourney | null>(
+    null,
+  );
+
+  const closeModal = useCallback(() => {
+    setIsModalVisible(false);
+  }, []);
+
+  const showModal = useCallback((selected) => {
+    setIsModalVisible(true);
+    setCareerJourney(selected);
+  }, []);
+
+  const handleSave = useCallback(
+    (payload) => dispatch(createCareerJourney(payload)),
+    [dispatch],
+  );
+
+  const handleUpdate = useCallback(
+    (payload) => dispatch(updateCareerJourney(payload)),
+    [dispatch],
+  );
+
+  const handleSubmit = (values: object): void => {
+    (careerJourney ? handleUpdate(values) : handleSave(values))
+      .unwrap()
+      .then(() => {
+        navigate(MentorMenteeRoute.PROFILE);
+      })
+      .catch((err: Error) => {
+        NotificationManager.error(err.message);
+      });
+  };
 
   return (
-    <AddSection title="Career journey" onButtonClick={showModal}>
+    <AddSection
+      title="Career journey"
+      onButtonClick={(): void => showModal(null)}
+    >
       <>
         {careerJourneyData.map((item, i) => (
-          <CareerCard
-            key={i}
-            position={item.position}
-            company={item.company}
-            startDate={item.startDate}
-            endDate={item.endDate}
-          />
+          <CareerCard key={i} careerJourney={item} onEdit={showModal} />
         ))}
-        <CareerJourneyForm
-          show={isModalVisible}
-          title={'Edit career journey'}
-          onClose={closeModal}
-        />
+        {isModalVisible && (
+          <CareerJourneyForm
+            careerJourney={careerJourney}
+            onClose={closeModal}
+            onSubmit={handleSubmit}
+          />
+        )}
       </>
     </AddSection>
   );
--- frontend/src/components/profile/components/profile-info/profile-main/education-section.tsx
+++ frontend/src/components/profile/components/profile-info/profile-main/education-section.tsx
@@ -1,43 +1,78 @@
-import { useCallback, useState } from 'hooks/hooks';
+import { NotificationManager } from 'react-notifications';
+import {
+  useAppDispatch,
+  useAppSelector,
+  useCallback,
+  useEffect,
+  useNavigate,
+  useState,
+} from 'hooks/hooks';
 import AddSection from '../add-section/add-section';
 import EducationCard from '../education-card/education-card';
 import { EducationForm } from '../../profile-edit/profile-edit';
-
-const educationData = [
-  {
-    id: '1',
-    specialization: 'Computer Science',
-    university: 'Lviv Polytechnic National University',
-    degree: 'Masters',
-    startDate: new Date('2021-12-17T03:24:00'),
-    endDate: new Date(),
-  },
-];
+import {
+  createEducation,
+  getAllEducations,
+  updateEducation,
+} from 'store/education/actions';
+import { Education } from '../interfaces';
+import { MentorMenteeRoute } from 'common/enums/mentor-mentee-route/mentor-mentee-route.enum';
 
 const EducationSection: React.FC = () => {
-  const [isModalVisible, setIsModalVisible] = useState(false);
+  const navigate = useNavigate();
+  const dispatch = useAppDispatch();
+  const educationData = useAppSelector((state) => state.education.educations);
 
-  const closeModal = useCallback(() => setIsModalVisible(false), []);
-  const showModal = useCallback(() => setIsModalVisible(true), []);
+  useEffect(() => {
+    dispatch(getAllEducations());
+  }, []);
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [education, setEducation] = useState<Education | null>(null);
+
+  const closeModal = useCallback(() => {
+    setIsModalVisible(false);
+  }, []);
+
+  const showModal = useCallback((selected) => {
+    setIsModalVisible(true);
+    setEducation(selected);
+  }, []);
+
+  const handleSave = useCallback(
+    (payload) => dispatch(createEducation(payload)),
+    [dispatch],
+  );
+
+  const handleUpdate = useCallback(
+    (payload) => dispatch(updateEducation(payload)),
+    [dispatch],
+  );
+
+  const handleSubmit = (values: object): void => {
+    (education ? handleUpdate(values) : handleSave(values))
+      .unwrap()
+      .then(() => {
+        navigate(MentorMenteeRoute.PROFILE);
+      })
+      .catch((err: Error) => {
+        NotificationManager.error(err.message);
+      });
+  };
 
   return (
-    <AddSection title="Education" onButtonClick={showModal}>
+    <AddSection title="Education" onButtonClick={(): void => showModal(null)}>
       <>
         {educationData.map((item, i) => (
-          <EducationCard
-            key={i}
-            specialization={item.specialization}
-            university={item.university}
-            degree={item.degree}
-            startDate={item.startDate}
-            endDate={item.endDate}
-          />
+          <EducationCard key={i} education={item} onEdit={showModal} />
         ))}
-        <EducationForm
-          show={isModalVisible}
-          title={'Edit education'}
-          onClose={closeModal}
-        />
+        {isModalVisible && (
+          <EducationForm
+            education={education}
+            onClose={closeModal}
+            onSubmit={handleSubmit}
+          />
+        )}
       </>
     </AddSection>
   );
