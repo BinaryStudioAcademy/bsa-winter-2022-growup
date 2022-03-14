bee4775efc3e62deae1a23216ea0236ee323a61e
Karyna <kharinakaryna@gmail.com>
2022-03-14 11:22:53 +0200

    fix: added select flag to user entity

--- backend/src/data/entities/user.ts
+++ backend/src/data/entities/user.ts
@@ -11,7 +11,7 @@ export class User extends AbstractEntity {
   @Column({ type: 'varchar', length: 50 })
   email: string;
 
-  @Column({ type: 'varchar', length: 100 })
+  @Column({ type: 'varchar', length: 100, select: false })
   password: string;
 
   @Column({ type: 'varchar', length: 250, nullable: true })
