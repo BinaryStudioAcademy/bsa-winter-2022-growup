a556a355f7c10004b84726a02ee3f5acd6037ae9
Iskenderov Viacheslav <s25112003@gmail.com>
2022-03-14 14:37:22 +0200

    fix: seeder fixed

--- backend/src/data/seed-data/opportunity.data.ts
+++ backend/src/data/seed-data/opportunity.data.ts
@@ -2,19 +2,19 @@ export const opporunities = [
   {
     name: 'Learning JS',
     organization: 'Data System Opp',
-    startDate: 'March 20 2022',
+    startDate: new Date(3, 2, 2022),
     type: 'Project/Lecture',
   },
   {
     name: 'QA Testing Like God',
     organization: 'Company Query',
-    startDate: 'May 02 2022',
+    startDate: new Date(2, 4, 2022),
     type: 'Lectury/HomeWork',
   },
   {
     name: 'How to improve your bussiness skills',
     organization: 'Imporove Yourself',
-    startDate: 'June 12 2022',
+    startDate: new Date(2, 5, 2022),
     type: 'Lecture',
   },
 ];
