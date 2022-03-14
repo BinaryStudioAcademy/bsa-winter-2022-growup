e4efab405353cba0e55755a61e5fdedee5721eb2
twistedomelette <54509271+twistedomelette@users.noreply.github.com>
2022-03-14 14:25:39 +0200

    fix: remove unnecessary

--- backend/src/data/seeders/index.ts
+++ backend/src/data/seeders/index.ts
@@ -8,14 +8,12 @@ import OpportunitySeeder from './opportunity.seeder';
 import TagsSeeder from './tags.seeder';
 import OpportunityTagSeeder from './opportunity-tags.seeder';
 import SkillSeeder from './skills.seeder';
-import UserSkillSeeder from './user-skill.seeder';
 
 async function Connection(): Promise<void> {
   await createConnection(ormconfig);
   await CompanySeeder.execute();
   await UserSeeder.execute();
   await SkillSeeder.execute();
-  await UserSkillSeeder.execute();
   await OpportunitySeeder.execute();
   await TagsSeeder.execute();
   await OpportunityTagSeeder.execute();
