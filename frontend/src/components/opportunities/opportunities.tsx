4816428f3ae64e9b0fb865a5c81c9d571630be89
Iskenderov Viacheslav <s25112003@gmail.com>
2022-03-21 10:04:36 +0200

    fix: wrong merge && added opportunity sort button

--- frontend/src/components/opportunities/opportunities.tsx
+++ frontend/src/components/opportunities/opportunities.tsx
@@ -3,7 +3,8 @@ import {
   useAppSelector,
   useCallback,
   useEffect,
-  useNavigate,
+  useState,
+  // useNavigate,
 } from 'hooks/hooks';
 import { NotificationManager } from 'react-notifications';
 import * as opportunityActions from 'store/opportunities/actions';
@@ -12,12 +13,15 @@ import OpportunityItem from '../main-page/opportunities/opportunity-item';
 import AddSection from '../profile/add-section/add-section';
 import OpportunityForm from './opportunity-form';
 import Follow from './follow';
-import isFirstLogged from 'helpers/check-is-first-logged';
+// import isFirstLogged from 'helpers/check-is-first-logged';
+import { Dropdown } from 'react-bootstrap';
+import { SortOption } from 'store/opportunities/common';
 
 const Opportunities: React.FC = () => {
+  const [sort, setSort] = useState<SortOption | null>(null);
   const dispatch = useAppDispatch();
-  const user = useAppSelector((store) => store.profile.user);
-  const navigate = useNavigate();
+  // const user = useAppSelector((store) => store.profile.user);
+  // const navigate = useNavigate();
   const isLoaded = useAppSelector((state) => state.opportunities.isLoaded);
   const isShowModal = useAppSelector(
     (state) => state.opportunities.isShowModal,
@@ -26,9 +30,9 @@ const Opportunities: React.FC = () => {
     (state) => state.opportunities.opportunities,
   );
 
-  useEffect(() => {
-    isFirstLogged({ user, navigate });
-  }, [user]);
+  // useEffect(() => {
+  //   isFirstLogged({ user, navigate });
+  // }, [user]);
 
   useEffect(() => {
     if (!isLoaded) {
@@ -60,16 +64,45 @@ const Opportunities: React.FC = () => {
       });
   };
 
+  const handleSort = (option: SortOption): void => {
+    dispatch(opportunityActions.sortOpportunities(option));
+    setSort(option);
+  };
+
   return (
     <AddSection
       title="Opportunities"
       buttonName={'Add opportunity'}
       onAdd={showModal}
     >
+      <Dropdown>
+        <Dropdown.Toggle
+          as="button"
+          className="btn btn-outline-gu-pink btn-responsive btn-hover-gu-white fw-bold fs-5 border-2"
+        >
+          {sort ? sort : 'Sort'}
+        </Dropdown.Toggle>
+        <Dropdown.Menu>
+          <Dropdown.Item
+            onClick={(): void => handleSort(SortOption.ORGANIZATION)}
+          >
+            Organization
+          </Dropdown.Item>
+          <Dropdown.Item onClick={(): void => handleSort(SortOption.PROGRAM)}>
+            Program
+          </Dropdown.Item>
+          <Dropdown.Item onClick={(): void => handleSort(SortOption.DATE)}>
+            Date
+          </Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
       {opportunityData.map((item: IOpportunity, index: number) => (
-        <div className="mt-3 px-3 py-3 rounded-1 shadow-lg border border-1 bg-gu-white d-flex">
+        <div
+          key={index}
+          className="mt-3 px-3 py-3 rounded-1 shadow-lg border border-1 bg-gu-white d-flex"
+        >
           <OpportunityItem
-            key={index}
             id={item.id}
             name={item.name}
             organization={item.organization}
