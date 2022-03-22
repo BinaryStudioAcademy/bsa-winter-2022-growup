6a2930ab93c3bc574e8b10046a3e91ad365377bf
Iskenderov Viacheslav <s25112003@gmail.com>
2022-03-22 21:18:29 +0200

    fix: uncommented

--- frontend/src/helpers/check-is-first-logged/index.ts
+++ frontend/src/helpers/check-is-first-logged/index.ts
@@ -1,4 +1,4 @@
-// import { MentorMenteeRoute } from 'common/enums/mentor-mentee-route/mentor-mentee-route.enum';
+import { MentorMenteeRoute } from 'common/enums/mentor-mentee-route/mentor-mentee-route.enum';
 import { IUser } from 'common/interfaces/user/user';
 import { NavigateFunction } from 'react-router-dom';
 
@@ -6,16 +6,15 @@ interface Props {
   user: IUser | null;
   navigate: NavigateFunction;
 }
-const isFirstLogged = (_props: Props): void => {
-  return;
-  // if (user && !user.firstName) {
-  //   navigate(`${MentorMenteeRoute.SETTINGS_PROFILE}/1`);
-  //   return;
-  // }
-  // if (user && !user.isCompleteTest) {
-  //   navigate(`${MentorMenteeRoute.SETTINGS_PROFILE}/2`);
-  //   return;
-  // }
+const isFirstLogged = ({ user, navigate }: Props): void => {
+  if (user && !user.firstName) {
+    navigate(`${MentorMenteeRoute.SETTINGS_PROFILE}/1`);
+    return;
+  }
+  if (user && !user.isCompleteTest) {
+    navigate(`${MentorMenteeRoute.SETTINGS_PROFILE}/2`);
+    return;
+  }
 };
 
 export default isFirstLogged;
