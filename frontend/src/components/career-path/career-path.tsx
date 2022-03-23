ba88b63c7e34feebf0a24488d5662487a91bd0d7
Karyna <kharinakaryna@gmail.com>
2022-03-23 15:32:06 +0200

    fix: updated styles

--- frontend/src/components/career-path/career-path.tsx
+++ frontend/src/components/career-path/career-path.tsx
@@ -2,11 +2,14 @@ import LevelFlow from './flow/level-flow';
 import LevelDescription from './level-description/level-description';
 import { nodes } from './level-description/data/nodes';
 import { edges } from './level-description/data/edges';
+import './styles.scss';
 
 const CareerPath: React.FC = () => {
   return (
-    <div className="d-flex">
-      <LevelFlow nodes={nodes} edges={edges} />
+    <div className="career-path d-flex">
+      <div className="w-100 h-100">
+        <LevelFlow nodes={nodes} edges={edges} />
+      </div>
       <LevelDescription />
     </div>
   );
--- frontend/src/components/career-path/level-description/level-description.tsx
+++ frontend/src/components/career-path/level-description/level-description.tsx
@@ -1,23 +1,24 @@
 import './level-description.scss';
+import { v4 as uuidv4 } from 'uuid';
 import { technicalSkills } from './data/technical-skills';
 import TechnicalSkills from './technical-skills';
 
 const LevelDescription: React.FC = () => {
   return (
-    <div className="level-description bg-white overflow-hidden">
+    <div className="level-description bg-white ms-3">
       <div className="level-description__title d-grid bg-gu-blue">
         <span className="fs-5 fw-bold text-white">Level</span>
         <span className="level-description__subtitle fs-6">
           33 / 35 skills archived
         </span>
       </div>
-      <div className="level-description__content position-relative h-100 overflow-auto">
+      <div className="level-description__content bg-white position-relative overflow-auto">
         <p className="fs-5 fw-bold">Technical skills</p>
         {technicalSkills.map(({ name, skills }) => (
-          <>
+          <div key={uuidv4()}>
             <p className="level-description__subtitle fs-6 fw-bold">{name}</p>
             <TechnicalSkills skills={skills} />
-          </>
+          </div>
         ))}
       </div>
     </div>
