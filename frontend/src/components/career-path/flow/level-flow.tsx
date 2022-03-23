d392c28736bd55947e1e80146763e717fe386d96
Karyna <kharinakaryna@gmail.com>
2022-03-23 15:35:52 +0200

    fix: made the career path page responsive

--- frontend/src/components/career-path/flow/level-flow.tsx
+++ frontend/src/components/career-path/flow/level-flow.tsx
@@ -1,5 +1,7 @@
 import React, { useMemo } from 'react';
-import ReactFlow, { Edge, Node } from 'react-flow-renderer';
+import { useState, useWindowSize } from 'hooks/hooks';
+import ReactFlow, { Edge, Node, ReactFlowInstance } from 'react-flow-renderer';
+import { LEVEL_WIDTH, MAX_LEVEL_FLOW_WIDTH } from './common/constants';
 import LevelNode from './level-node';
 import UserNode from './user-node';
 import './level-flow.scss';
@@ -15,23 +17,39 @@ const LevelFlow: React.FC<Props> = ({ nodes, edges }) => {
     [],
   );
 
+  useWindowSize();
+
+  const [instance, setInstance] = useState<ReactFlowInstance>();
+  const levelFlow = document.getElementById('level-flow');
+  const levelFlowHeight = edges.length * LEVEL_WIDTH;
+  const levelFlowWidth = levelFlow?.clientWidth || 1;
+  const prefZoom = levelFlowWidth / MAX_LEVEL_FLOW_WIDTH;
+
+  if (instance) {
+    levelFlow && (levelFlow.style.height = `${levelFlowHeight * prefZoom}px`);
+    instance.fitView();
+    const { x, zoom } = instance.getViewport();
+    instance.setViewport({ x, y: 25, zoom });
+  }
+
   return (
     <ReactFlow
+      id="level-flow"
+      onInit={(flowInstance: ReactFlowInstance): void =>
+        setInstance(flowInstance)
+      }
       nodes={nodes}
       edges={edges}
       nodeTypes={nodeTypes}
-      fitView
-      maxZoom={1}
-      minZoom={0.3}
       snapToGrid={true}
       draggable={false}
       panOnDrag={false}
       nodesDraggable={false}
       connectOnClick={false}
       zoomOnPinch={false}
-      zoomOnScroll={true}
+      zoomOnScroll={false}
       zoomOnDoubleClick={false}
-      className="level-flow bg-white me-3"
+      className="level-flow bg-white"
       attributionPosition="bottom-left"
     />
   );
