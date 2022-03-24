02a49aacd13bb04936959fd9a1419d7d06da29e5
Iskenderov Viacheslav <s25112003@gmail.com>
2022-03-24 22:08:16 +0200

    fix: migration fix

--- backend/src/data/migrations/1648121179728-user-role-string.ts
+++ backend/src/data/migrations/1648121179728-user-role-string.ts
@@ -8,33 +8,41 @@ interface IUserRole {
   role: RoleType;
 }
 
+interface IUser {
+  id: string;
+  role: RoleType;
+}
+
 export class userRoleString1648121179728 implements MigrationInterface {
   name = 'userRoleString1648121179728';
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.startTransaction();
 
-    await queryRunner.query(
-      'CREATE TYPE "public"."user_role_enum" AS ENUM(\'Mentor\', \'Mentee\', \'Admin\')',
-    );
-    await queryRunner.query(
-      'ALTER TABLE "user" ADD "role" "public"."user_role_enum" NOT NULL',
-    );
+    await queryRunner.query('CREATE TYPE "public"."user_role_enum" AS ENUM(\'Mentor\', \'Mentee\', \'Admin\')');
+    await queryRunner.query('ALTER TABLE "user" ADD "role" "public"."user_role_enum"');
 
-    const roles: IUserRole[] = await queryRunner.query(
-      'SELECT * FROM "user_role"',
-    );
+    const roles: IUserRole[] = await queryRunner.query('SELECT * FROM "user_role"');
 
-    asyncForEach(async (role) => {
-      await queryRunner.query(
-        `UPDATE "user" SET role = '${role.role}' WHERE id = '${role.userId}'`,
-      );
+    await asyncForEach(async (role) => {
+      await queryRunner.query(`UPDATE "user" SET role = '${role.role}' WHERE id = '${role.userId}'`);
     }, roles);
 
-    await queryRunner.commitTransaction();
+    await queryRunner.query('ALTER TABLE "user" ALTER COLUMN "role" SET NOT NULL');
+
+    await queryRunner.query('ALTER TABLE "user_role" DROP CONSTRAINT "FK_ab40a6f0cd7d3ebfcce082131fd"');
+    await queryRunner.query('DROP TABLE "user_role"');
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('CREATE TABLE "user_role" ("id" uuid NOT NULL DEFAULT uuid_generate_v4(), "createdAt" TIMESTAMP NOT NULL DEFAULT now(), "updatedAt" TIMESTAMP NOT NULL DEFAULT now(), "deletedAt" TIMESTAMP, "role" "public"."user_role_role_enum" NOT NULL, "userId" uuid, CONSTRAINT "PK_fb2e442d14add3cefbdf33c4561" PRIMARY KEY ("id"))');
+    await queryRunner.query('ALTER TABLE "user_role" ADD CONSTRAINT "FK_ab40a6f0cd7d3ebfcce082131fd" FOREIGN KEY ("userId") REFERENCES "user"("id") ON DELETE NO ACTION ON UPDATE NO ACTION');
+
+    const users: IUser[] = await queryRunner.query('SELECT * FROM "user"');
+
+    await asyncForEach(async (user) => {
+      await queryRunner.query(`INSERT INTO "user_role" ("role", "userId") VALUES ('${user.role}', '${user.id}')`);
+    }, users);
+
     await queryRunner.query('ALTER TABLE "user" DROP COLUMN "role"');
     await queryRunner.query('DROP TYPE "public"."user_role_enum"');
   }
