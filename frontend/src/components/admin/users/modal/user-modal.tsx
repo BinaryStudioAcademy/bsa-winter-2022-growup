e4da6931b2075a0d9fe9231366f4712b24074fbb
Iskenderov Viacheslav <s25112003@gmail.com>
2022-03-24 10:38:00 +0200

    fix: comments fixed

--- frontend/src/components/admin/users/modal/user-modal.tsx
+++ frontend/src/components/admin/users/modal/user-modal.tsx
@@ -7,15 +7,17 @@ type Props = {
   onClose: () => void;
 };
 
-const UserModal: React.FC<Props> = memo(({ show, onClose }) => (
-  <Modal
-    show={show}
-    onClose={onClose}
-    title="Add User"
-    className="d-flex flex-column gap-4"
-  >
-    <UserForm onSubmit={onClose} />
-  </Modal>
-));
+const UserModal: React.FC<Props> = memo(({ show, onClose }) => {
+  return (
+    <Modal
+      show={show}
+      onClose={onClose}
+      title="Add Tags"
+      className="d-flex flex-column gap-4"
+    >
+      <UserForm onSubmit={onClose} />
+    </Modal>
+  );
+});
 
 export default UserModal;
