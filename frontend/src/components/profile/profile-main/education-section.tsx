2066cd29e3dff7f3d386437036bf27636e788a8f
Pavel Kramarenko <pavelk@sweetsoft.com.ua>
2022-03-24 20:03:53 +0200

    add sorting by date

--- frontend/src/components/profile/profile-main/education-section.tsx
+++ frontend/src/components/profile/profile-main/education-section.tsx
@@ -1,5 +1,6 @@
 import { NotificationManager } from 'react-notifications';
 import { Row } from 'react-bootstrap';
+import { ArrowDown, ArrowUp } from 'react-bootstrap-icons';
 import {
   useAppDispatch,
   useAppSelector,
@@ -19,15 +20,21 @@ import { EducationForm } from '../components/profile-edit/profile-edit';
 
 const EducationSection: React.FC = () => {
   const dispatch = useAppDispatch();
-  const educationData = useAppSelector((state) => state.education.educations);
+
+  const [isModalVisible, setIsModalVisible] = useState(false);
+  const [education, setEducation] = useState<IEducation | null>(null);
+  const [sort, setSort] = useState<boolean>(false);
+  const [viewAll, setViewAll] = useState<boolean>(false);
+
+  const { educations } = useAppSelector((state) => state.education);
+  const isLessTwo = educations && educations.length <= 2;
 
   useEffect(() => {
     dispatch(getAllEducations());
   }, []);
 
-  const [isModalVisible, setIsModalVisible] = useState(false);
-  const [education, setEducation] = useState<IEducation | null>(null);
-
+  const onSortByAsc = (): void => setSort(!sort);
+  const onViewAll = (): void => setViewAll(!viewAll);
   const closeModal = useCallback(() => {
     setIsModalVisible(false);
   }, []);
@@ -59,17 +66,45 @@ const EducationSection: React.FC = () => {
   };
 
   const educationItems = (): JSX.Element[] | JSX.Element => {
-    if (!educationData.length) {
+    if (!educations.length) {
       return <Row className="w-100 justify-content-center">No Data</Row>;
     }
-    return educationData.map((item, i) => (
-      <EducationCard key={i} education={item} onEdit={showModal} />
-    ));
+    if (educations.length < 2) {
+      return <EducationCard education={educations[0]} onEdit={showModal} />;
+    }
+
+    const sortByStartDateAsc = (a: IEducation, b: IEducation): number => {
+      if (a.startDate < b.startDate) {
+        return sort ? -1 : 1;
+      }
+      if (a.startDate > b.startDate) {
+        return sort ? 1 : -1;
+      }
+      return 0;
+    };
+
+    const to = viewAll ? educations.length : 2;
+
+    return [...educations]
+      .sort(sortByStartDateAsc)
+      .slice(0, to)
+      .map((item, i) => (
+        <EducationCard key={i} education={item} onEdit={showModal} />
+      ));
   };
 
   return (
     <AddSection title="Education" onAdd={(): void => showModal(null)}>
       <>
+        {educations.length > 1 && (
+          <span
+            onClick={onSortByAsc}
+            className="notifications__view-all d-flex align-items-center justify-content-end me-2 mb-2 mt-2 fs-6 text-gu-blue"
+          >
+            <span className="me-1">{sort ? 'sort desc' : 'sort asc'}</span>
+            {sort ? <ArrowDown /> : <ArrowUp />}
+          </span>
+        )}
         {educationItems()}
         {isModalVisible && (
           <EducationForm
@@ -77,6 +112,15 @@ const EducationSection: React.FC = () => {
             onClose={closeModal}
             onSubmit={handleSubmit}
           />
+        )}
+        {!isLessTwo && (
+          <span
+            onClick={onViewAll}
+            className="notifications__view-all d-flex align-items-center justify-content-end me-2 mb-2 mt-2 fs-6 text-gu-blue"
+          >
+            <span className="me-1">{viewAll ? 'hide' : 'view all'}</span>
+            {viewAll ? <ArrowUp /> : <ArrowDown />}
+          </span>
         )}
       </>
     </AddSection>
