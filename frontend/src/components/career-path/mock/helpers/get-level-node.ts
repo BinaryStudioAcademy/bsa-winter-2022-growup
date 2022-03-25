26cf1c2b7a99ac70d4359d54a0f039267cb674bf
Karyna <kharinakaryna@gmail.com>
2022-03-25 13:32:37 +0200

    fix: added mock data

--- frontend/src/components/career-path/mock/helpers/get-level-node.ts
+++ frontend/src/components/career-path/mock/helpers/get-level-node.ts
@@ -1,13 +1,6 @@
 import { v4 as uuidv4 } from 'uuid';
 import { INode } from '../common/interfaces';
 
-// const acquired = (y: number): number => {
-//   if (y === 0) return 32;
-//   if (y === 120) return 25;
-//   if (y === 240) return 16;
-//   return 5;
-// };
-
 export const getLevelNode = (
   level: string,
   y: number,
