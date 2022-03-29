b43fca0f6089b052c105801e65ba906f82e1c78d
Iskenderov Viacheslav <s25112003@gmail.com>
2022-03-29 19:39:18 +0300

    fix: comments fixed

--- frontend/src/common/enums/validation/company-validation-rule.enum.ts
+++ frontend/src/common/enums/validation/company-validation-rule.enum.ts
@@ -1,6 +1,9 @@
 const CompanyValidationRule = {
   NAME_MIN: 3,
+  NAME_MAX: 50,
+
   DESCRIPTION_MIN: 3,
+  DESCRIPTION_MAX: 500,
 };
 
 export { CompanyValidationRule };
--- frontend/src/validation-schemas/company/company.validation-schema.ts
+++ frontend/src/validation-schemas/company/company.validation-schema.ts
@@ -8,17 +8,21 @@ const company = Joi.object({
   'name': Joi.string()
     .trim()
     .min(CompanyValidationRule.NAME_MIN)
+    .max(CompanyValidationRule.NAME_MAX)
     .required()
     .messages({
       'string.min': CompanyValidationMessage.NAME_MIN,
+      'string.max': CompanyValidationMessage.NAME_MAX,
       'string.empty': CompanyValidationMessage.NAME_EMPTY,
     }),
   'description': Joi.string()
     .trim()
     .allow('')
     .min(CompanyValidationRule.DESCRIPTION_MIN)
+    .max(CompanyValidationRule.DESCRIPTION_MAX)
     .messages({
       'string.min': CompanyValidationMessage.DESCRIPTION_MIN,
+      'string.max': CompanyValidationMessage.DESCRIPTION_MAX,
     }),
 });
 
