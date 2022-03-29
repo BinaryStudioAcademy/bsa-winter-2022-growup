b9bab1848403d352f0f0d757a468cd17f1327fe8
Karyna <kharinakaryna@gmail.com>
2022-03-29 20:55:20 +0300

    fix: changed button 'Add OKR' position on OKR page

--- frontend/src/components/okr/tabs/okr-list.tsx
+++ frontend/src/components/okr/tabs/okr-list.tsx
@@ -1,59 +1,39 @@
-import React, { useState } from 'react';
 import { IOkr } from 'common/interfaces/okr';
 import OrkItem from '../workspace-item';
-
-import { Button } from 'components/common/common';
-import OkrModal from '../modal';
-
 import './styles.scss';
 
 interface Props {
   collection: IOkr[];
 }
 
-const OkrList: React.FC<Props> = ({ collection }) => {
-  const [showModal, setShowModal] = useState(false);
+const OkrList: React.FC<Props> = ({ collection }) => (
+  <>
+    <div className="OKR-page d-flex flex-row flex-wrap">
+      {collection.map((okr: IOkr) => {
+        const objectives = okr.objectives;
+        let objectivesCounter = 0;
+        let resultsCounter = 0;
 
-  const openModal = (): void => setShowModal(true);
-  const closeModal = (): void => setShowModal(false);
-
-  return (
-    <>
-      <Button
-        variant="gu-pink"
-        className="text-gu-white mb-2 align-self-end"
-        onClick={openModal}
-      >
-        + Add OKR
-      </Button>
-      <div className="OKR-page d-flex flex-row flex-wrap">
-        {collection.map((okr: IOkr) => {
-          const objectives = okr.objectives;
-          let objectivesCounter = 0;
-          let resultsCounter = 0;
-
-          if (objectives) {
-            objectivesCounter += objectives.length;
-            resultsCounter = objectives.reduce((acc, objective) => {
-              if (objective.keyResults) {
-                acc += objective.keyResults.length;
-              }
-              return acc;
-            }, 0);
-          }
-          return (
-            <OrkItem
-              key={okr.id}
-              okr={okr}
-              objectivesCounter={objectivesCounter}
-              resultsCounter={resultsCounter}
-            />
-          );
-        })}
-      </div>
-      {showModal && <OkrModal showModal={showModal} closeModal={closeModal} />}
-    </>
-  );
-};
+        if (objectives) {
+          objectivesCounter += objectives.length;
+          resultsCounter = objectives.reduce((acc, objective) => {
+            if (objective.keyResults) {
+              acc += objective.keyResults.length;
+            }
+            return acc;
+          }, 0);
+        }
+        return (
+          <OrkItem
+            key={okr.id}
+            okr={okr}
+            objectivesCounter={objectivesCounter}
+            resultsCounter={resultsCounter}
+          />
+        );
+      })}
+    </div>
+  </>
+);
 
 export default OkrList;
