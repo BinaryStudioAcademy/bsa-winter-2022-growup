afc8e30927410f0e33de26f79d7e06704850750a
twistedomelette <54509271+twistedomelette@users.noreply.github.com>
2022-03-30 20:29:06 +0300

    feat: opportunity type and okr validations

--- frontend/src/components/okr/modal/update-objective-modal.tsx
+++ frontend/src/components/okr/modal/update-objective-modal.tsx
@@ -1,5 +1,10 @@
 import { Button, Modal, TextField } from 'components/common/common';
-import { useAppDispatch, useAppForm, useAppSelector } from 'hooks/hooks';
+import {
+  useAppDispatch,
+  useAppForm,
+  useAppSelector,
+  useState,
+} from 'hooks/hooks';
 import { Form } from 'react-bootstrap';
 import { useFieldArray } from 'react-hook-form';
 import { objectiveValidationSchema } from 'validation-schemas/validation-schemas';
@@ -24,6 +29,11 @@ const UpdateObjectiveModal: React.FC<Props> = ({
   objectiveId,
 }) => {
   const dispatch = useAppDispatch();
+  const [isValidResult, setIsValidResult] = useState<boolean[]>([false]);
+  const [isValidResultName, setIsValidResultName] = useState<boolean[]>([
+    false,
+  ]);
+  const [isValidName, setIsValidName] = useState<boolean>(false);
 
   const { okrs } = useAppSelector((state: RootState) => state.okr);
   const okr = okrs.find((okr) => okr.id === okrId);
@@ -50,22 +60,58 @@ const UpdateObjectiveModal: React.FC<Props> = ({
     control,
   });
 
-  const onSubmit = (data: ObjectiveValues): void => {
-    const keyValues = data.keyResults.map((res) => res.result);
-    const sumResult = keyValues.reduce((total, amount) => +total + +amount);
-    const objectiveValues = Math.round(sumResult / data.keyResults.length);
-    dispatch(
-      okrActions.updateObjective_async({
-        okrId,
-        objectiveId,
-        objectiveBody: {
-          name: data.name,
-          result: objectiveValues,
-        },
-        keyResults: data.keyResults,
-      }),
+  function validResult(el: number): boolean {
+    if (Math.floor(el) != el) return false;
+    return true;
+  }
+
+  function validName(el: string): boolean {
+    if (!el) return false;
+    if (el.length < 2 || el.length > 30) return false;
+    return true;
+  }
+
+  function valid(
+    objName: string,
+    keyResults: { name: string; result: number }[],
+  ): boolean {
+    validName(objName) ? setIsValidName(false) : setIsValidName(true);
+    const isNames = [...isValidResultName];
+    const isResults = [...isValidResult];
+    const keyNames = keyResults.map((key) => key.name);
+    const results = keyResults.map((key) => key.result);
+
+    keyNames.forEach((name, index) => (isNames[index] = !validName(name)));
+    results.forEach(
+      (result, index) => (isResults[index] = !validResult(result)),
     );
-    closeModal();
+    setIsValidResultName(isNames);
+    setIsValidResult(isResults);
+
+    if (isValidName) return false;
+    if (isNames.some((name) => name) || isResults.some((result) => result))
+      return false;
+    return true;
+  }
+
+  const onSubmit = (data: ObjectiveValues): void => {
+    if (valid(data.name, data.keyResults)) {
+      const keyValues = data.keyResults.map((res) => res.result);
+      const sumResult = keyValues.reduce((total, amount) => +total + +amount);
+      const objectiveValues = Math.round(sumResult / data.keyResults.length);
+      dispatch(
+        okrActions.updateObjective_async({
+          okrId,
+          objectiveId,
+          objectiveBody: {
+            name: data.name,
+            result: objectiveValues,
+          },
+          keyResults: data.keyResults,
+        }),
+      );
+      closeModal();
+    }
   };
   return (
     <Modal
@@ -84,6 +130,13 @@ const UpdateObjectiveModal: React.FC<Props> = ({
           errors={errors}
           name="name"
         />
+        {isValidName ? (
+          <span className="fs-6 text-gu-pink error react-error position-relative">
+            Name is required
+          </span>
+        ) : (
+          true
+        )}
         {fields.map((field, index) => {
           return (
             <Form.Group
@@ -97,14 +150,36 @@ const UpdateObjectiveModal: React.FC<Props> = ({
                   errors={errors}
                   name={`keyResults.${index}.name`}
                 />
+                {isValidResultName[index] ? (
+                  <span
+                    className="fs-6 text-gu-pink error react-error position-relative"
+                    id={`keyResults.${index}.result`}
+                  >
+                    Name is required
+                  </span>
+                ) : (
+                  true
+                )}
               </div>
-              <TextField
-                label={'Score 0/1'}
-                control={control}
-                errors={errors}
-                type="number"
-                name={`keyResults.${index}.result`}
-              />
+              <div>
+                <TextField
+                  label={'Score 0/1'}
+                  control={control}
+                  errors={errors}
+                  type="number"
+                  name={`keyResults.${index}.result`}
+                />
+                {isValidResult[index] ? (
+                  <span
+                    className="fs-6 text-gu-pink error react-error position-relative"
+                    id={`keyResults.${index}.result`}
+                  >
+                    Result is required
+                  </span>
+                ) : (
+                  true
+                )}
+              </div>
               <Button
                 onClick={(): void => remove(index)}
                 className="border-0 bg-transparent text-gu-black hover-pink"
@@ -115,7 +190,17 @@ const UpdateObjectiveModal: React.FC<Props> = ({
           );
         })}
         <Button
-          onClick={(): void => append({ name: '', result: 0 })}
+          onClick={(): void => {
+            setIsValidResult((isValidResult): boolean[] => [
+              ...isValidResult,
+              false,
+            ]);
+            setIsValidResultName((isValidResultName): boolean[] => [
+              ...isValidResultName,
+              false,
+            ]);
+            append({ name: '', result: 0 });
+          }}
           className="ms-2 border-0 bg-transparent text-gu-pink fw-bold"
         >
           + Add key result
