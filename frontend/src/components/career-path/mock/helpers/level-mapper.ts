20213357cb424703f366527befa6e578ada1eba8
Karyna <kharinakaryna@gmail.com>
2022-03-31 15:02:24 +0300

    fix: removed mock data

--- frontend/src/components/career-path/mock/helpers/level-mapper.ts
+++
@@ -1,8 +0,0 @@
-import { ILevel, ILevelMap } from '../common/interfaces';
-
-export const levelMapper = (levels: ILevel[]): ILevelMap => {
-  return levels.reduce(
-    (result, item: ILevel) => ({ ...result, [item.id]: item }),
-    {},
-  );
-};
