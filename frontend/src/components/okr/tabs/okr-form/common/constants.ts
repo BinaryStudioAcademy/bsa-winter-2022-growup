d7c795e4cca2876b302834287eec8fc603dbf737
Karyna <kharinakaryna@gmail.com>
2022-04-01 12:16:36 +0300

    feat: created okr edit form modal

---
+++ frontend/src/components/okr/tabs/okr-form/common/constants.ts
@@ -0,0 +1,9 @@
+import { OkrPayloadKey } from 'common/enums/enums';
+
+const DEFAULT_OKR_PAYLOAD = {
+  [OkrPayloadKey.NAME]: '',
+  [OkrPayloadKey.START_DATE]: null,
+  [OkrPayloadKey.END_DATE]: null,
+};
+
+export { DEFAULT_OKR_PAYLOAD };
