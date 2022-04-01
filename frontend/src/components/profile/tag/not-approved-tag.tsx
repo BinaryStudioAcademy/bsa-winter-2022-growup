c02eb22fe9cb7c71d83e3e27a5ab9eab73c63f1d
twistedomelette <54509271+twistedomelette@users.noreply.github.com>
2022-04-01 11:34:16 +0300

    feat: removed spaces

--- frontend/src/components/profile/tag/not-approved-tag.tsx
+++ frontend/src/components/profile/tag/not-approved-tag.tsx
@@ -1,10 +1,7 @@
 import './styles.scss';
 
 const NotApprovedTag: React.FC = ({ children }) => (
-  <label className="tag not-approved-tag text-gu-white fs-6">
-    {' '}
-    {children}{' '}
-  </label>
+  <label className="tag not-approved-tag text-gu-white fs-6">{children}</label>
 );
 
 export default NotApprovedTag;
